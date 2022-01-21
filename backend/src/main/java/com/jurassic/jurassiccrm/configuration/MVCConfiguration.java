77cbb572c781a78d6b7b591c8822735ec8494f23
Fitisov Dmitrii <fitisovdmtr@gmail.com>
2022-01-21 18:43:10 +0300

    Add swagger support to automatically create FE api

--- backend/src/main/java/com/jurassic/jurassiccrm/configuration/MVCConfiguration.java
+++ backend/src/main/java/com/jurassic/jurassiccrm/configuration/MVCConfiguration.java
@@ -2,13 +2,13 @@ package com.jurassic.jurassiccrm.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
@@ -29,6 +29,15 @@ public class MVCConfiguration implements WebMvcConfigurer {
                 .addResourceHandler("/webjars/**", "/templates/**", "/static/**")
                 .addResourceLocations("/webjars/", "webjars/static", "/templates", "/static")
                 .resourceChain(false);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
