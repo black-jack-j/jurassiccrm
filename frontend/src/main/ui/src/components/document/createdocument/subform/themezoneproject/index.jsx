e8eeed4f7b252b00d9811758907c863a0b9b6b23
Fitisov Dmitrii <fitisovdmtr@gmail.com>
2022-01-27 21:15:43 +0300

    # JSCRM-2 FE Basic Document Markup
    Implement theme zone project localizations

--- frontend/src/main/ui/src/components/document/createdocument/subform/themezoneproject/index.jsx
+++ frontend/src/main/ui/src/components/document/createdocument/subform/themezoneproject/index.jsx
@@ -8,17 +8,29 @@ import {
 } from "./fieldNames";
 import React from "react";
 import {FieldArray} from "formik";
+import {useTranslation} from "react-i18next";
 
 export const ThemeZoneProjectSubform = props => {
 
+    const {t} = useTranslation('translation', {keyPrefix: 'crm.document.form.create.theme_zone_project.field'})
+
     return (
         <>
-            <Input name={THEME_ZONE_PROJECT_NAME} {...props}/>
-            <Input name={THEME_ZONE_PROJECT_MANAGER} {...props}/>
-            <FieldArray name={DINOSAURS_SELECTOR} />
+            <Input name={THEME_ZONE_PROJECT_NAME}
+                   placeholder={t(`${THEME_ZONE_PROJECT_NAME}.placeholder`)}
+                   {...props}/>
+
+            <Input name={THEME_ZONE_PROJECT_MANAGER}
+                   placeholder={t(`${THEME_ZONE_PROJECT_MANAGER}`)}
+                   {...props}/>
+
+            <FieldArray name={DINOSAURS_SELECTOR}/>
+            TODO: {t(`${DINOSAURS_SELECTOR}.title`)}
             <FieldArray name={AVIARIES_SELECTOR}/>
+            TODO: {t(`${AVIARIES_SELECTOR}.title`)}
             <FieldArray name={DECORATIONS_SELECTOR}/>
-        </>
+            TODO: {t(`${DECORATIONS_SELECTOR}.title`)}
+            </>
     )
 
 }
