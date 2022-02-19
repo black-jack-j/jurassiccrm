fd26bf7ae973087c7eaa184627c4f5dbd609b285
black-jack-j <fitisovdmtr@gmail.com>
2022-02-19 22:29:27 +0300

    # JSCRM-27 Document editor
    feature(frontend): disable aviary/dinosaur type updates and creation/incubation dates updates

--- frontend/src/main/ui/src/components/update-document-form/utils.js
+++ frontend/src/main/ui/src/components/update-document-form/utils.js
@@ -1,7 +1,4 @@
 import {CreateDocumentDocumentTypeEnum as DocumentTypeEnum} from "../../generatedclient/apis";
-import {SuspendableResearchDataFormContainer} from "../research-data-form/suspendable-research-data-form-container";
-import {SuspendableDinosaurPassportFormContainer} from "../dinosaur-passport-form/suspendable-dinosaur-passport-form-container";
-import {SuspendableAviaryPassportFormContainer} from "../aviary-passport-form/suspendable-aviary-passport-form-container";
 import {SuspendableThemeZoneProjectFormContainer} from "../theme-zone-project-form/suspendable-theme-zone-project-form-container";
 import {SuspendableTechnologicalMapFormContainer} from "../technologicalmap-form/suspendable-technologicalmap-form-container";
 
@@ -16,13 +13,16 @@ import deserializeAP from "../aviary-passport-form/deserialize";
 import deserializeTM from "../technologicalmap-form/deserialize";
 import deserializeDP from "../dinosaur-passport-form/deserialize";
 import deserializeTZP from "../theme-zone-project-form/deserialize";
+
 import {SuspendableUpdateResearchDataFormContainer} from "../update-research-data-form/suspendable-update-research-data-form-container";
+import {SuspendableUpdateAviaryPassportFormContainer} from "../aviary-passport-form/suspendable-update-aviary-passport-form-container";
+import {SuspendableUpdateDinosaurPassportFormContainer} from "../dinosaur-passport-form/suspendable-update-dinosaur-passport-form-container";
 
 
 const formMap = {
     [DocumentTypeEnum.ResearchData]: [SuspendableUpdateResearchDataFormContainer, updateResearch, deserializeResearch],
-    [DocumentTypeEnum.DinosaurPassport]: [SuspendableDinosaurPassportFormContainer, updateDinosaur, deserializeDP],
-    [DocumentTypeEnum.AviaryPassport]: [SuspendableAviaryPassportFormContainer, updateAviary, deserializeAP],
+    [DocumentTypeEnum.DinosaurPassport]: [SuspendableUpdateDinosaurPassportFormContainer, updateDinosaur, deserializeDP],
+    [DocumentTypeEnum.AviaryPassport]: [SuspendableUpdateAviaryPassportFormContainer, updateAviary, deserializeAP],
     [DocumentTypeEnum.ThemeZoneProject]: [SuspendableThemeZoneProjectFormContainer, updateTZP, deserializeTZP],
     [DocumentTypeEnum.TechnologicalMap]: [SuspendableTechnologicalMapFormContainer, updateTM, deserializeTM]
 }
