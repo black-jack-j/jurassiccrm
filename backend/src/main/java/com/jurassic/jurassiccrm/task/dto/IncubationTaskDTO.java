0d0cc15db48f3ad13aa3c34866a76c106610166f
black-jack-j <fitisovdmtr@gmail.com>
2022-02-27 19:21:01 +0300

    # JSCRM-33-FE
    fix(backend): add validator for SimpleEntityTO id

--- backend/src/main/java/com/jurassic/jurassiccrm/task/dto/IncubationTaskDTO.java
+++ backend/src/main/java/com/jurassic/jurassiccrm/task/dto/IncubationTaskDTO.java
@@ -5,6 +5,8 @@ import com.jurassic.jurassiccrm.common.dto.SimpleEntityOutputTO;
 import com.jurassic.jurassiccrm.dinosaur.dao.DinosaurTypeRepository;
 import com.jurassic.jurassiccrm.task.model.incubation.IncubationTask;
 import com.jurassic.jurassiccrm.validation.existence.NullOrExists;
+import com.jurassic.jurassiccrm.validation.groups.OnCreate;
+import com.jurassic.jurassiccrm.validation.groups.OnUpdate;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +18,10 @@ import lombok.NoArgsConstructor;
 @ApiModel(parent = TaskTO.class)
 public class IncubationTaskDTO extends TaskTO {
 
-    @NullOrExists(repository = DinosaurTypeRepository.class)
+    @NullOrExists(
+            repository = DinosaurTypeRepository.class,
+            groups = {OnCreate.class, OnUpdate.class}
+    )
     private SimpleEntityOutputTO dinosaurType;
 
     public static IncubationTaskDTO fromTask(IncubationTask task) {
