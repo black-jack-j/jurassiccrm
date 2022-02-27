b2b85debe6b11d7fb9ab2f3aa90436b37cad6351
Алексей Леванович <levanovich_aa@icloud.com>
2022-02-28 01:25:35 +0300

    Tasks Selenium Tests

---
+++ backend/src/test/java/com/jurassic/jurassiccrm/seleniumtests/TasksTests.java
@@ -0,0 +1,104 @@
+package com.jurassic.jurassiccrm.seleniumtests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static java.lang.Thread.sleep;
+
+@Disabled
+public class TasksTests {
+
+    private static WebDriver chromedriver;
+    private static WebDriver firefoxdriver;
+
+    private static final String randString1 = java.util.UUID.randomUUID().toString().substring(0, 9);
+    private static final String randString2 = java.util.UUID.randomUUID().toString().substring(0, 9);
+
+    public static void loginJCRM(WebDriver driver) {
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.name("username")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@value='Sign In']")).click();
+    }
+
+    public static void createTask(WebDriver driver, String taskName) throws InterruptedException {
+        driver.findElement(By.linkText("Task")).click();
+        driver.findElement(By.cssSelector(".plus")).click();
+        sleep(1000);
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("name")).sendKeys(taskName);
+        driver.findElement(By.xpath("//input[@placeholder='Assignee']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Assignee']")).sendKeys("admin");
+        sleep(100);
+        driver.findElement(By.xpath("//div[@class='result']")).click();
+        driver.findElement(By.xpath("//div[text()='Priority']")).click();
+        driver.findElement(By.xpath("//span[text()='normal']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Goal']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Goal']")).sendKeys("test goal");
+        driver.findElement(By.xpath("//textarea[@placeholder='Description']")).click();
+        driver.findElement(By.xpath("//textarea[@placeholder='Description']")).sendKeys("test desc");
+        driver.findElement(By.xpath("//button[text()='Create']")).click();
+        sleep(1000);
+        driver.findElement(By.cssSelector(".refresh")).click();
+        sleep(100);
+        String xpathAssert = "//h3[text()='taskName']".replace("taskName", taskName);
+        assert driver.findElements(By.xpath(xpathAssert)).size() == 1;
+    }
+
+    public static void changeStatus(WebDriver driver, String taskName) throws InterruptedException {
+        driver.findElement(By.linkText("Task")).click();
+        String xpathAssert = "//h3[text()='taskName']".replace("taskName", taskName);
+        sleep(1000);
+        driver.findElement(By.cssSelector(".refresh")).click();
+        sleep(100);
+        driver.findElement(By.xpath(xpathAssert)).click();
+        sleep(100);
+        driver.findElement(By.xpath("//div[text()='Move to']")).click();
+        driver.findElement(By.xpath("//div[text()='IN_PROGRESS']")).click();
+        assert driver.findElements(By.xpath("//div[text()='IN_PROGRESS']")).size() == 1;
+        sleep(100);
+        driver.findElement(By.xpath("//div[text()='Move to']")).click();
+        driver.findElement(By.xpath("//div[text()='RESOLVED']")).click();
+        assert driver.findElements(By.xpath("//div[text()='RESOLVED']")).size() == 1;
+        sleep(100);
+        driver.findElement(By.xpath("//div[text()='Move to']")).click();
+        driver.findElement(By.xpath("//div[text()='CLOSED']")).click();
+        assert driver.findElements(By.xpath("//div[text()='CLOSED']")).size() == 1;
+    }
+
+    @BeforeAll
+    public static void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        chromedriver = new ChromeDriver();
+        firefoxdriver = new FirefoxDriver();
+        loginJCRM(chromedriver);
+        loginJCRM(firefoxdriver);
+    }
+
+    @Test
+    @Order(1)
+    public void createNewTask() throws InterruptedException {
+        createTask(chromedriver, randString1);
+        createTask(firefoxdriver, randString2);
+    }
+
+    @Test
+    @Order(2)
+    public void changeTaskStatus() throws InterruptedException {
+        changeStatus(chromedriver, randString1);
+        changeStatus(firefoxdriver, randString2);
+    }
+
+    @AfterAll
+    public static void closeBrowser() {
+        chromedriver.close();
+        firefoxdriver.close();
+    }
+}
---
+++ backend/src/test/java/com/jurassic/jurassiccrm/seleniumtests/WikiTests.java
@@ -0,0 +1,104 @@
+package com.jurassic.jurassiccrm.seleniumtests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static java.lang.Thread.sleep;
+
+@Disabled
+public class WikiTests {
+
+    private static WebDriver chromedriver;
+    private static WebDriver firefoxdriver;
+
+    private static void loginJCRM(WebDriver driver) {
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.name("username")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@value='Sign In']")).click();
+    }
+
+    private static void createWiki(WebDriver driver, String pageName) throws InterruptedException {
+        driver.get("http://localhost:8080/wiki/home/");
+        driver.findElement(By.linkText("Создать новую страницу Вики")).click();
+        driver.findElement(By.id("title")).click();
+        driver.findElement(By.id("title")).sendKeys(pageName);
+        driver.findElement(By.cssSelector("textarea")).click();
+        driver.findElement(By.cssSelector("textarea")).sendKeys("Test");
+        driver.findElement(By.id("submit")).click();
+        sleep(1000);
+        String xpathAssert = "//*[contains(text(), 'pageName')]".replace("pageName", pageName);
+        assert driver.findElement(By.xpath(xpathAssert)).isDisplayed();
+    }
+
+    private static void editWiki(WebDriver driver, String pageName) throws InterruptedException {
+        driver.get("http://localhost:8080/wiki/admin");
+        String xpath = "//a[contains(@href, '/wiki/edit?pageName=Test')]".replace("pageName=Test", "pageName=" + pageName);
+        driver.findElement(By.xpath(xpath)).click();
+        driver.findElement(By.cssSelector("textarea")).click();
+        driver.findElement(By.cssSelector("textarea")).sendKeys("TestTestTest");
+        driver.findElement(By.xpath("//input[@value='Применить изменения']")).click();
+        sleep(1000);
+        driver.get("http://localhost:8080/wiki/home");
+        driver.findElement(By.linkText(pageName)).click();
+        assert driver.findElement(By.xpath("//p")).getText().equals("TestTestTestTest");
+    }
+
+    private static void deleteWiki(WebDriver driver, String pageName) throws InterruptedException {
+        driver.get("http://localhost:8080/wiki/admin");
+        String xpath = "//a[@id='Test']".replace("Test", pageName);
+        driver.findElement(By.xpath(xpath)).click();
+        Alert javascriptAlert = driver.switchTo().alert();
+        javascriptAlert.accept();
+        sleep(2000);
+        javascriptAlert = driver.switchTo().alert();
+        javascriptAlert.accept();
+        sleep(2000);
+        driver.get("http://localhost:8080/wiki/home");
+        sleep(2000);
+        assert driver.findElements(By.linkText(pageName)).size() == 0;
+    }
+
+    @BeforeAll
+    public static void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        chromedriver = new ChromeDriver();
+        firefoxdriver = new FirefoxDriver();
+        loginJCRM(chromedriver);
+        loginJCRM(firefoxdriver);
+    }
+
+    @Test
+    @Order(1)
+    public void createNewWiki() throws InterruptedException {
+        createWiki(chromedriver, "Test");
+        createWiki(firefoxdriver, "Test1");
+    }
+
+    @Test
+    @Order(2)
+    public void editWiki() throws InterruptedException {
+        editWiki(chromedriver, "Test");
+        editWiki(firefoxdriver, "Test1");
+    }
+
+    @Test
+    @Order(3)
+    public void deleteWiki() throws InterruptedException {
+        deleteWiki(chromedriver, "Test");
+        deleteWiki(firefoxdriver, "Test1");
+    }
+
+    @AfterAll
+    public static void closeBrowser() {
+        chromedriver.close();
+        firefoxdriver.close();
+    }
+}
