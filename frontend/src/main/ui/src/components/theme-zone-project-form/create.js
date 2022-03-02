dacf2de898fe94ac8411d2a195d2d0424e6d5776
black-jack-j <fitisovdmtr@gmail.com>
2022-03-02 08:21:12 +0300

    # JSCRM-40
    fix(backend, frontend): fix non-creating theme zone project

--- frontend/src/main/ui/src/components/theme-zone-project-form/create.js
+++ frontend/src/main/ui/src/components/theme-zone-project-form/create.js
@@ -1,5 +1,31 @@
-import {CreateDocumentDocumentTypeEnum as DocumentTypeEnum} from "../../generatedclient/apis";
-import serialize from "./serialize";
+import {
+    CreateDocumentDocumentTypeEnum as DocumentType,
+    CreateDocumentDocumentTypeEnum as DocumentTypeEnum
+} from "../../generatedclient/apis";
+import {
+    AVIARIES_SELECTOR, DECORATIONS_SELECTOR,
+    DINOSAURS_SELECTOR,
+    DOCUMENT_NAME,
+    THEME_ZONE_PROJECT_MANAGER,
+    THEME_ZONE_PROJECT_NAME
+} from "./fieldNames";
+
+const BASKET_TRANSFORMER = values => values.reduce((acc, val) => {
+    acc[val.item] = val.count
+    return acc
+}, {})
+
+const serialize = form => ({
+
+    name: form[DOCUMENT_NAME],
+    documentType: DocumentType.ThemeZoneProject,
+    projectName: form[THEME_ZONE_PROJECT_NAME],
+    managerId: form[THEME_ZONE_PROJECT_MANAGER],
+    dinosaurs: BASKET_TRANSFORMER(form[DINOSAURS_SELECTOR]),
+    aviaries: BASKET_TRANSFORMER(form[AVIARIES_SELECTOR]),
+    decorations: BASKET_TRANSFORMER(form[DECORATIONS_SELECTOR])
+
+})
 
 export const create = API => values => {
 
